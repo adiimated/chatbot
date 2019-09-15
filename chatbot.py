e1076291f99285eb1b0de79abdd7ba5da5fee40a
Adiimated <47184551+adiimated@users.noreply.github.com>
2019-09-15 07:59:42 -0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -72,4 +72,86 @@ for question in clean_answers:
         else:
             word2count[word] += 1
             
+#Creating two dictionaries that map the question words to a unique integer
+#Tokenization and filtering the non frequent words
+threshold = 20
+questionswords2int = {}
+word_number = 0
+for word,count in word2count.items():
+    if count >= threshold:
+        questionswords2int[word] = word_number
+        word_number += 1
+
+answerswords2int = {}
+word_number = 0
+for word,count in word2count.items():
+    if count >= threshold:
+        answerswords2int[word] = word_number
+        word_number += 1        
+
+#Adding the last tokens to these two dictionaries
+#Tokenization is the process of tokenizing or splitting a string, text into a list of tokens.
+#One can think of token as parts like a word is a token in a sentence, and a sentence is a token in a paragraph.
+tokens = ['<PAD>','<EOS>','<OUT>','<SOS>']
+for token in tokens:
+    questionswords2int[token] = len(questionswords2int) + 1                     #refer 13
+for token in tokens:
+    answerswords2int[token] = len(answerswords2int) + 1
+    
+#Creating the inverse dictionary of the answerswords2int dictionary 
+#Required in creating seq2seq model
+answersints2word = {w_i:w for w,w_i in answerswords2int.items()}                #refer 14
+
+#Adding the end of string token to the end of every answer
+for i in range(len(clean_answers)):
+    clean_answers[i] += ' <EOS>'
+#To denote end of answer[end of string]
+    
+#Translating all the questions and the answers into integers
+#And replacing all the words that were filtered out by <OUT>
+questions_into_int = []
+for question in clean_questions:
+    ints = []
+    for word in question.split():
+        if word not in questionswords2int:
+            ints.append(questionswords2int['<OUT>'])
+        else:
+            ints.append(questionswords2int[word])
+    questions_into_int.append(ints)
+    
+answers_into_int = []
+for answer in clean_answers:
+    ints = []
+    for word in answer.split():
+        if word not in answerswords2int:
+            ints.append(answerswords2int['<OUT>'])
+        else:
+            ints.append(answerswords2int[word])
+    answers_into_int.append(ints)
+
+#Sorting questions and answers by the lenghth of the question            
+#Speeds up the training and optimize it
+#Reduces padding during the training   #Refer 17 
+sorted_clean_questions = []
+sorted_clean_answers = []
+for length in range(1,25 + 1):
+    for i in enumerate(questions_into_int):
+        if len(i[1]) == length:
+            sorted_clean_questions.append(questions_into_int[i[0]])
+            sorted_clean_answers.append(answers_into_int[i[0]])
+            
+
+#Creating placeholders for inputs and the targets
+#A placeholder is simply a variable that we will assign data to at a later date. 
+#It allows us to create our operations and build our computation graph, without needing the data. 
+#In TensorFlowterminology, we then feed data into the graph through these placeholders.
+def model_inputs():
+    inputs = tf.placeholder(tf.int32,[None,None],name = 'input')
+    targets = tf.placeholder(tf.int32,[None,None],name = 'target')
+    lr = tf.placeholder(tf.float32,name = 'learning_rate')
+    keep_prob = tf.placeholder(tf.float32,name = 'keep_prob')
+    return inputs,targets,lr,keep_prob
+
+
+            
 
